84ba529b3148585ab7cce52490b5cfd9f0fe1cbc
Mohamed Abdul-Raouf <mohamed.a.raouf@icloud.com>
2022-06-18 12:55:01 +0200

    Initial Commit

---
+++ LuftbornTask/LuftbornTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuftbornTaskApp.swift
+//  LuftbornTask
+//
+//  Created by mohamed on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LuftbornTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
